3451ab7749224563bc826a22f236c4bcbb8a0f46
ashwith123 <ashwith9492@gmail.com>
2025-08-07 00:14:19 +0530

    Fix: Incorrect User model import in user routes (closes #47)

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../routes/user");
+const User = require("../models/user"); 
 const passport = require("passport");
 const session = require("express-session");
 
